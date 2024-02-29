7773addc9ef7340ad7309c7c78c1b8fe7be8699f
KaishAli <360mohdkaishali.cs@gmail.com>
2024-03-01 00:51:04 +0530

    done

--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -23,30 +23,49 @@ function CartPage() {
     const Gst = calculateGST(rest)
     const grandTotal = rest + Gst + 10
     return (
-        <div>
+        <div className="container">
             <h2>Cart</h2>
             {cartItems.length === 0 ? (
                 <p>Your cart is empty</p>
             ) : (
                 <>
-                    <ul>
+                    <ul className="list-group mb-3">
                         {cartItems.map(item => (
-                            <li key={item.id}>
-                                {item.title} - ${item.price}
-                                <button onClick={() => removeFromCart(item)}>Remove</button>
+                            <li key={item.id} className="list-group-item d-flex justify-content-between align-items-center">
+                                <div className="d-flex align-items-center">
+                                    <img src={item.images[0]} alt={item.title} style={{ width: "50px", height: "50px", marginRight: "10px" }} />
+                                    <div>
+                                        <h6 className="mb-0">{item.title}</h6>
+                                        <p className="mb-0">${item.price}</p>
+                                        <span>{item.description}</span>
+                                    </div>
+                                </div>
+                                <button className="btn btn-danger" onClick={() => removeFromCart(item)}>Remove</button>
                             </li>
                         ))}
                     </ul>
-                    <div>
-                        <div>Total :{rest}</div>
-                        <div>GST 28% :{Gst}</div>
-                        <div>Shipping Charge : $10</div>
-                        <div>Grand Total : {grandTotal}</div>
+                    <div className="bg-light p-3">
+                        <div className="d-flex justify-content-between">
+                            <div>Total:</div>
+                            <div>${rest}</div>
+                        </div>
+                        <div className="d-flex justify-content-between">
+                            <div>GST 28%:</div>
+                            <div>${Gst}</div>
+                        </div>
+                        <div className="d-flex justify-content-between">
+                            <div>Shipping Charge:</div>
+                            <div>$10</div>
+                        </div>
+                        <div className="d-flex justify-content-between">
+                            <div>Grand Total:</div>
+                            <div>${grandTotal}</div>
+                        </div>
                     </div>
                 </>
-
             )}
         </div>
+
     );
 }
 
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -5,6 +5,7 @@ import ProductCard from './ProductCard';
 import { useSelector, useDispatch } from 'react-redux';
 import { SearchBar } from './SearchBar';
 import { fetchProductData } from '../API/getProductData';
+import './ProductCard.css'
 function HomePage() {
     const cartItems = useSelector(state => state.cart.items);
     console.log(cartItems.length);
@@ -12,14 +13,13 @@ function HomePage() {
     const [productsCopy, setProductsCopy] = useState([]);
     const navigate = useNavigate()
     const [minPrice, setMinPrice] = useState(0);
-    const [maxPrice, setMaxPrice] = useState(1000); 
+    const [maxPrice, setMaxPrice] = useState(1000);
     const [category, setCategory] = useState('');
     const [isLoading, setIsLoading] = useState(true);
     useEffect(() => {
         fetchData()
     }, []);
 
-    // Function to fetch data from the API
     async function fetchData() {
         try {
             const response = await fetchProductData();
@@ -27,7 +27,7 @@ function HomePage() {
             if (response && response.data && response.data.products) {
                 setProducts(response.data.products);
                 setProductsCopy(response.data.products);
-                
+
             } else {
                 console.error('No data found in the API response.');
 
@@ -44,57 +44,85 @@ function HomePage() {
         return isInPriceRange && isMatchingCategory;
     });
 
-    console.log(products);
-    function handleCartNav() {
-        navigate("/cart")
-    }
-
-
-
-
+    // console.log(products);
+    // function handleCartNav() {
+    //     navigate("/cart")
+    // }
     return (
-        <div>
-            <SearchBar products={products} setProducts={setProducts} productsCopy={productsCopy} />
-            <div style={{ height: "60px", width: '100%', display: 'flex', justifyContent: 'flex-end', alignItems: 'center', background: "aliceBlue" }}>
-                <span onClick={handleCartNav}>
-                    <img src="" alt='shoppingcart' style={{ width: "25px", height: "25px", position: "relative" }} />
-                    <span style={{ position: "absolute", zIndex: "49", top: "0px", right: "0px", color: "red", background: "lightskyblue", borderRadius: "50%", width: "15px", height: "21px", cursor: "pointer" }}>
-                        {cartItems.length}
-                    </span>
-                </span>
-                <div className="filters">
+        <>
+            <div className='' style={{ marginBottom: "100px" }}>
+                <nav className="navbar navbar-expand-lg navbar-light bg-secondary fixed-top">
+                    <div className="container-fluid">
+                        <a className="navbar-brand" href="#">Thunder</a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0 gap-5">
+                                <li className="nav-item">
+                                    <label className="nav-link">Price Range:</label>
+                                    <select className="form-select" value={`${minPrice}-${maxPrice}`} onChange={e => {
+                                        const value = e.target.value;
+                                        let min, max;
+                                        if (value === "0-2000") {
+                                            min = 0;
+                                            max = 2000;
+                                        } else {
+                                            [min, max] = value.split('-').map(Number);
+                                            max = max === 1000 ? Infinity : max; // Handle "1000+" option
+                                        }
+                                        setMinPrice(min);
+                                        setMaxPrice(max);
+                                    }}>
+                                        <option value="0-2000">All</option>
+                                        <option value="0-100">1 - 100</option>
+                                        <option value="100-200">100 - 200</option>
+                                        <option value="200-500">200 - 500</option>
+                                        <option value="500-1000">500 - 1000</option>
+                                        <option value="1000-">1000+</option>
+                                    </select>
+                                </li>
 
-                    <label>Price Range:</label>
-                    <select value={`${minPrice}-${maxPrice}`} onChange={e => {
-                        const [min, max] = e.target.value.split('-').map(Number);
-                        setMinPrice(min);
-                        setMaxPrice(max === 1000 ? Infinity : max); // Handle "1000+" option
-                    }}><option value="0-2000">All</option>
-                        <option value="0-100">1 - 100</option>
-                        <option value="100-200">100 - 200</option>
-                        <option value="200-500">200 - 500</option>
-                        <option value="500-1000">500 - 1000</option>
-                        <option value="1000-">1000+</option>
-                    </select>
-                    <label>Category:</label>
-                    <select value={category} onChange={e => setCategory(e.target.value)}>
-                        <option value="">All</option>
-                        <option value="smartphones">Mobile</option>
-                        <option value="laptops">laptop</option>
-                        <option value="groceries">groceries</option>
-                        <option value="skincare">skincare</option>
-                        <option value="home-decoration">home-decoration</option>
-                        {/* Add more categories as needed */}
-                    </select>
+                                <li className="nav-item">
+                                    <label className="nav-link">Category:</label>
+                                    <select className="form-select" value={category} onChange={e => setCategory(e.target.value)}>
+                                        <option value="">All</option>
+                                        <option value="smartphones">Mobile</option>
+                                        <option value="laptops">laptop</option>
+                                        <option value="groceries">groceries</option>
+                                        <option value="skincare">skincare</option>
+                                        <option value="home-decoration">home-decoration</option>
+                                        {/* Add more categories as needed */}
+                                    </select>
+                                </li>
+                            </ul>
+                            <div className="d-flex">
+                                <SearchBar products={products} setProducts={setProducts} productsCopy={productsCopy} />
+                            </div>
+                            <div className="mx-2">
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" style={{ width: "25px", height: "25px", position: "relative" }} width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                                        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
+                                    </svg>
+                                    <sup> <span style={{ fontSize: "15px", position: "absolute", zIndex: "49", top: "0px", right: "0px", color: "white", borderRadius: "50%", width: "15px", height: "21px", cursor: "pointer" }}>
+                                        {cartItems.length}
+                                    </span></sup>
+
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+            <div className="">
+                <h1>Product Catalog</h1>
+                <div className="cantainer-card" style={{}}>
+                    {filteredProducts.map(product => (
+                        <ProductCard key={product.id} product={product} />
+                    ))}
                 </div>
             </div>
-            <h1>Product Catalog</h1>
-            <div className="product-grid">
-                {filteredProducts.map(product => (
-                    <ProductCard  key={product.id} product={product} />
-                ))}
-            </div>
-        </div >
+        </>
     );
 }
 
